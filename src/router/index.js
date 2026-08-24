2b6d1237baf1b1153d4824469a503a2d6203d615
Victorious <your-email@example.com>
2026-08-24 21:11:36 +0300

    Added routes

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Intake from '../views/IntakeView.vue'
+import Bays from '../views/BaysView.vue'
+import Parts from '../views/PartsView.vue'
+import Reports from '../views/ReportsView.vue'
+import Job from '../views/JobView.vue'
+import NotFound from '../views/Error.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Intake',
+    component: Intake
+  },
+  {
+    path: '/bays',
+    name: 'Bays',
+    component: Bays
+  },
+  {
+    path: '/parts',
+    name: 'Parts',
+    component: Parts
+  },
+  {
+    path: '/reports',
+    name: 'Reports',
+    component: Reports
+  },
+  {
+  path: '/job/:plate',
+  name: 'Job',
+  component: Job
+}
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
